33a0265ebede17b22e901a6fc3222b68dc10af6a
Ilya Petrov <dnamix1@gmail.com>
2020-04-20 10:28:21 +0300

    app top structure with theme embedded with ThemeProvider of the styled-components utility functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from 'styled-components';
 
+import { GlobalStyles, theme } from './styles';
+import { Content } from './components/styles/content';
+import { Title } from './components/styles/title';
+import { Card } from './components/styles/card';
+import Grid from './components/grid/index';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content>
+      <Title>
+        <span role="img" aria-label="logo">
+          🥋
+        </span>{' '}
+        Redoku
+      </Title>
+      <Card>
+        <Grid />
+      </Card>
+    </Content>
+  </ThemeProvider>,
+  document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
