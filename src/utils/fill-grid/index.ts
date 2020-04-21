1b329b20f5690d78c743974c2808144efa2b4b7d
Ilya Petrov <dnamix1@gmail.com>
2020-04-21 12:50:40 +0300

    fill grid function with jest tests

---
+++ src/utils/fill-grid/index.ts
@@ -0,0 +1,45 @@
+import { NUMBERS, GRID } from 'typings';
+import shuffle from 'utils/shuffle';
+import isInRow from 'utils/is-in/row';
+import isInCol from 'utils/is-in/col';
+import identifyWorkingSquare from 'utils/identify-square';
+import isInSquare from 'utils/is-in/square';
+import checkGrid from 'utils/check-grid';
+
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+/**
+ * A backtracking/recursive function to check all the possible combinations of numbers a solution is found
+ * @param grid  9X9 Sudoku Grid
+ */
+function fillGrid(grid: GRID) {
+  let row = 0;
+  let col = 0;
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9);
+    col = i % 9;
+
+    if (grid[row][col] === 0) {
+      shuffle(numbers);
+
+      for (let value of numbers) {
+        if (!isInRow({ grid, row, value }))
+          if (!isInCol({ col, grid, value })) {
+            const square = identifyWorkingSquare({ col, grid, row });
+            if (!isInSquare({ square, value })) {
+              grid[row][col] = value;
+              if (checkGrid(grid)) return true;
+              else if (fillGrid(grid)) return true;
+            }
+          }
+      }
+
+      break;
+    }
+  }
+
+  grid[row][col] = 0;
+}
+
+export default fillGrid;
