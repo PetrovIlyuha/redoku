778dfe18f79ca5693e8fe6d8c86f2fdcf25c4b3d
Ilya Petrov <dnamix1@gmail.com>
2020-04-21 12:52:19 +0300

    create full grid function and tests

---
+++ src/utils/create-full-grid/index.ts
@@ -0,0 +1,23 @@
+import fillGrid from 'utils/fill-grid';
+import { GRID } from 'typings';
+
+/**
+ * function to create a full valid sudoku grid
+ */
+function createFullGrid() {
+  const grid: GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  fillGrid(grid);
+  return grid;
+}
+
+export default createFullGrid;
