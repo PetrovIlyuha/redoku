a330b95ea9dc4d77a2eb1aa64048211c80490238
Ilya Petrov <dnamix1@gmail.com>
2020-04-22 12:17:59 +0300

    block selection functionality module

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,13 +1,41 @@
 import React, { FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Dispatch, AnyAction } from 'redux';
+import { IReducer } from 'reducers';
+import { INDEX } from '../../../typings';
 import { Container } from './styles';
+import { selectBlock } from 'reducers/actions';
+import { N } from 'typings';
 
 interface IProps {
-  colIndex: number;
-  rowIndex: number;
+  colIndex: INDEX;
+  rowIndex: INDEX;
+}
+
+interface IState {
+  isActive: boolean;
+  value: N;
 }
 
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container></Container>;
+  const state = useSelector<IReducer, IState>(
+    ({ workingGrid, selectedBlock }) => ({
+      isActive: selectedBlock
+        ? selectedBlock[0] === rowIndex && selectedBlock[1] === colIndex
+        : false,
+      value: workingGrid ? workingGrid[rowIndex][colIndex] : 0,
+    })
+  );
+  const dispatch = useDispatch<Dispatch<AnyAction>>();
+
+  function handleClick() {
+    if (!state.isActive) dispatch(selectBlock([rowIndex, colIndex]));
+  }
+  return (
+    <Container active={state.isActive} onClick={handleClick}>
+      {state.value === 0 ? '' : state.value}
+    </Container>
+  );
 };
 
 export default Block;
