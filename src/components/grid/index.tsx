4e9f90f70c437a8408b9235351d28979bd260e3b
Ilya Petrov <dnamix1@gmail.com>
2020-04-22 12:22:23 +0300

    wiring up reducer dispatch actions in the main game grid

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,11 +1,97 @@
-import React, { FC, Children } from 'react';
+import React, { FC, Children, useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import useMousetrap from 'react-hook-mousetrap';
+import { Dispatch, AnyAction } from 'redux';
 import { Container, Row } from './styles';
 import Block from './block';
-import createFullGrid from 'utils/create-full-grid';
+import { INDEX, BLOCK_COORDS, NUMBERS, N } from 'typings';
+import { createGrid, selectBlock, fillBlock } from 'reducers/actions';
+import { IReducer } from 'reducers';
 
+interface IState {
+  selectedBlock?: BLOCK_COORDS;
+  selectedValue: N;
+}
 const Grid: FC = () => {
-  const grid = createFullGrid();
-  console.log(grid);
+  const state = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  );
+  const dispatch = useDispatch<Dispatch<AnyAction>>();
+  const create = useCallback(() => dispatch(createGrid()), [dispatch]);
+  useEffect(() => {
+    create();
+  }, [create]);
+
+  const fill = useCallback(
+    (n: NUMBERS) => {
+      if (state.selectedBlock && state.selectedValue === 0) {
+        dispatch(fillBlock(n, state.selectedBlock));
+      }
+    },
+    [dispatch, state.selectedBlock]
+  );
+
+  const moveDown = () => {
+    if (state.selectedBlock && state.selectedBlock[0] < 8) {
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] + 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      );
+    }
+  };
+  const moveLeft = () => {
+    if (state.selectedBlock && state.selectedBlock[1] > 0) {
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] - 1) as INDEX,
+        ])
+      );
+    }
+  };
+  const moveRight = () => {
+    if (state.selectedBlock && state.selectedBlock[1] < 8) {
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] + 1) as INDEX,
+        ])
+      );
+    }
+  };
+  const moveUp = () => {
+    if (state.selectedBlock && state.selectedBlock[0] > 0) {
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] - 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      );
+    }
+  };
+
+  useMousetrap('1', () => fill(1));
+  useMousetrap('2', () => fill(2));
+  useMousetrap('3', () => fill(3));
+  useMousetrap('4', () => fill(4));
+  useMousetrap('5', () => fill(5));
+  useMousetrap('6', () => fill(6));
+  useMousetrap('7', () => fill(7));
+  useMousetrap('8', () => fill(8));
+  useMousetrap('9', () => fill(9));
+  useMousetrap('down', moveDown);
+  useMousetrap('left', moveLeft);
+  useMousetrap('right', moveRight);
+  useMousetrap('up', moveUp);
+
   return (
     <Container>
       {Children.toArray(
@@ -13,7 +99,10 @@ const Grid: FC = () => {
           <Row>
             {Children.toArray(
               [...Array(9)].map((_, colIndex) => (
-                <Block rowIndex={rowIndex} colIndex={colIndex} />
+                <Block
+                  rowIndex={rowIndex as INDEX}
+                  colIndex={colIndex as INDEX}
+                />
               ))
             )}
           </Row>
