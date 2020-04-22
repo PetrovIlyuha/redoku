5b4d05aff3e4ab81fab70bef85ff38712c6b0363
Ilya Petrov <dnamix1@gmail.com>
2020-04-22 12:20:10 +0300

    grid container styled

--- src/components/grid/block/styles/index.ts
+++ src/components/grid/block/styles/index.ts
@@ -1,16 +1,21 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-  ${({ theme }) => css`
+interface IProps {
+  active?: boolean;
+}
+
+export const Container = styled.div<IProps>`
+  ${({ active, theme }) => css`
     align-items: center;
-    background-color: ${theme.colors.white};
+    background-color: ${active ? 'red' : theme.colors.white};    
     border: 1px solid ${theme.colors.black};
+    color: ${active ? 'white' : theme.colors.black};
     cursor: pointer;
     display: flex;
     flex-grow: 1;
     flex-shrink: 0;
     flex-basis: 0;
-    font-size: 20px;
+    font-size: ${active ? '24px' : '20px'};
     font-weight: bold;
     height: 40px;
     justify-content: center;
