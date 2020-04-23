fe09cb163bb0ca14a6250f160c0ea5eaabcfd8dd
Ilya Petrov <dnamix1@gmail.com>
2020-04-23 13:21:24 +0300

    added PersistGate wrapper function in the main Index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,36 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import configureStore from './core/configure-store';
+import { register } from './core/service-worker';
 
 import { GlobalStyles, theme } from 'styles';
 import { Content } from 'components/styles/content';
 import { Title } from 'components/styles/title';
 import { Card } from 'components/styles/card';
 import Grid from 'components/grid/index';
+import NewButton from 'components/new-button';
+import Numbers from 'components/numbers';
 
-const store = configureStore();
+const { persistor, store } = configureStore();
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Provider store={store}>
-      <Content>
-        <Title>
-          {''}
-          <span role="img" aria-label="logo">
-            ⚛
-          </span>{' '}
-          Redoku
-          <span role="img" aria-label="logo">
-            🥋
-          </span>{' '}
-        </Title>
-        <Card>
-          <Grid />
-        </Card>
-      </Content>
+      <PersistGate loading={null} persistor={persistor}>
+        <Content>
+          <Title>
+            {''}
+            <span role="img" aria-label="logo">
+              ⚛
+            </span>{' '}
+            Redoku
+            <span role="img" aria-label="logo">
+              🥋
+            </span>{' '}
+          </Title>
+          <Card>
+            <NewButton />
+            <Grid />
+            <Numbers />
+          </Card>
+        </Content>
+      </PersistGate>
     </Provider>
   </ThemeProvider>,
   document.querySelector('#root')
 );
+
+register();
